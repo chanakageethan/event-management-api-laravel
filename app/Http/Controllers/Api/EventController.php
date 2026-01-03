a6124cd448637c5e637d471a3363e69aa18e1551
geethanchanaka <chanakageethan@gmail.com>
2026-01-03 13:27:41 +0530

    create event

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -20,7 +21,20 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time',
+            ]),
+            'user_id' => 1,
+
+        ]);
+
+        return $event;
+
     }
 
     /**
