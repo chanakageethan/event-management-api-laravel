d4216a0a28becf40ac587bb5c18b3c8ce0c60f24
geethanchanaka <chanakageethan@gmail.com>
2026-01-09 23:24:04 +0530

    Authorization with Gates

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -7,7 +7,8 @@ use App\Http\Resources\EventResource;
 use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
- 
+use Illuminate\Support\Facades\Gate;
+
 class EventController extends Controller 
 {
     use CanLoadRelationships;
@@ -87,6 +88,13 @@ class EventController extends Controller
      */
     public function update(Request $request, Event $event )
     {
+
+            if(Gate::denies('update-event')){
+                abort(403,'You are not authrozied to update this event');
+            }
+
+   
+            Gate::authorize('update-event', $event);
 
             $event->update(
                 $request->validate([
