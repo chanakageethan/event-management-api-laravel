74c66cc07ab7898687ddf8f1479226733ce9ebcf
geethanchanaka <chanakageethan@gmail.com>
2026-01-15 14:08:42 +0530

    queues

--- app/Notifications/EventReminder.php
+++ app/Notifications/EventReminder.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EventReminder extends Notification
+class EventReminder extends Notification implements ShouldQueue
 {
     use Queueable;
 
